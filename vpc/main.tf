677a21488521bdb6e9844e2b267d3830643c9a86
Lohithakolla <93385567+Lohithakolla@users.noreply.github.com>
2022-02-22 08:45:38 -0800

    Update main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     region = "us-east-1"
-    access_key = ""
-    secret_key = ""
+    access_key = "AKIA2PRCPUFGXQDHPGHG"
+    secret_key = "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"
 }
 module "vpc_creation" {
     source = "./vpc_creation1"
