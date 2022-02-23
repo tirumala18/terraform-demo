d96fcd1c9886edb7ddaf85e0bcbbeec215ce1d03
Lohithakolla <93385567+Lohithakolla@users.noreply.github.com>
2022-02-22 21:49:40 -0800

    Update main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -27,7 +27,7 @@ module "NAT"{
   name="ex-nat"
 }
 module "route_table" {
- source = "./routetables" 
+ source = "./routetable" 
  vpc_id = module.vpc.vpc_id
  igw_id = module.IGW.igw_id
  nat_id = module.NAT.nat_id
